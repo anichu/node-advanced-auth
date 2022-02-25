f20e6c0e18e6d28ece48c8b07aa8dc9ff832d182
MD.Anis Molla <41534160+anichu@users.noreply.github.com>
2022-02-25 11:58:36 +0600

    for forgot password

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,11 @@
-const express = require("express");
-const router = express.Router();
-
 const {
 	register,
 	login,
 	forgotpassword,
 	resetpassword,
 } = require("../controllers/auth");
+const express = require("express");
+const router = express.Router();
 
 router.route("/register").post(register);
 
@@ -14,6 +13,6 @@ router.route("/login").post(login);
 
 router.route("/forgotpassword").post(forgotpassword);
 
-router.route("/resetpassword/:resetToken").put(resetpassword);
+router.route("/passwordreset/:resetToken").put(resetpassword);
 
 module.exports = router;
